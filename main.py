92e34b7c36bf851269cdf84e4c956c52b45c9ea1
jacmei <jackeymei0@gmail.com>
2019-11-13 07:11:34 -0500

    Tidying up code

--- main.py
+++ main.py
@@ -212,9 +212,6 @@ def main():
     except FileNotFoundError as e:
         print(e)
     except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
         exc_info = sys.exc_info()
         traceback.print_exception(*exc_info)
         print(e)
